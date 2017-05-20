f3cfc018c942651fce224f14ef3ac52f674af835
fongelias <fongelias@gmail.com>
2017-05-19 22:05:01 -0400

    Added mocha

---
+++ test/testApp.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+var grid = require('../dev/js/components/grid.js');
+
+describe("Grid", function() {
+	describe("get/setMatrix", function() {
+		it("", function() {
+			assert(true, 'Its false')
+		})
+	});
+	describe("randomizeMatrix", function() {
+		it("", function() {
+			assert(true,'Its false');
+
+
+		});
+	});
+	describe("get/setValue", function() {
+		it("", function() {
+			assert(true,'Its false');
+
+
+		});
+	});
+	describe("neighbors", function() {
+		it("", function() {
+			assert(true,'Its false');
+
+
+		});
+	});
+	describe("evalState", function() {
+		it("", function() {
+			assert(true,'Its false');
+
+
+		});
+	});
+	describe("nextGeneration", function() {
+		it("", function() {
+			assert(true,'Its false');
+
+
+		});
+	});
+
+})
