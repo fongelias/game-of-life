b2a6c442376df0338da170d49af39a24fceb0714
fongelias <fongelias@gmail.com>
2017-05-20 17:32:17 -0400

    Added a test for Grid.getMatrix()

--- test/testApp.js
+++ test/testApp.js
@@ -1,10 +1,18 @@
 var assert = require('assert');
-var grid = require('../dev/js/components/grid.js');
+var Grid = require('../dev/js/components/grid.js');
 
 describe("Grid", function() {
+	var test_grid = new Grid();
+
 	describe("get/setMatrix", function() {
-		it("", function() {
-			assert(true, 'Its false')
+		it("respond with set matrix", function() {
+			test_grid.setMatrix(3,3);
+			var test_matrix = test_grid.getMatrix();
+
+			var test_bool = test_matrix.length === 3;
+			test_bool = test_matrix[0].length === 3;
+
+			assert(test_bool);
 		})
 	});
 	describe("randomizeMatrix", function() {
